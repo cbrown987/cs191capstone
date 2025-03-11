4b06cd770f14df2726e15e2de6b6607c3261b4c1
cooperb <bcooper987@protonmail.ch>
2025-03-11 14:20:28 -0500

    General Improvements

---
+++ app/components/menu/MenuWrapper.tsx
@@ -0,0 +1,10 @@
+import { Suspense } from "react";
+import { Menu } from "@/app/components/menu/menu";
+
+export const MenuWrapper = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Menu />
+    </Suspense>
+  );
+};
