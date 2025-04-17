cd8d0891026389158f69b002babd93e5bd710644
cooperb <bcooper987@protonmail.ch>
2025-04-17 16:03:26 -0500

    upadate docker to working version

--- deployment/setup.sh
+++ deployment/setup.sh
@@ -7,7 +7,7 @@ sudo apt-get update && apt-get install -y \
     && apt-get clean \
     && rm -rf /var/lib/apt/lists/*
 
-#git clone https://github.com/cbrown987/cs191capstone.git
+git clone https://github.com/cbrown987/cs191capstone.git
 cd cs191capstone/deployment || return
 
 docker-compose up -d --build
