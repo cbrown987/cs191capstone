805ab4030bbdcc51be5633f9a82fefb33bb8f540
cooperb <bcooper987@protonmail.ch>
2025-04-17 15:03:43 -0500

    add deployment start

--- api/index.py
+++ api/index.py
@@ -138,4 +138,4 @@ async def ai_chat(message: str, context: str = ""):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=5328)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
---
+++ deployment/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+sudo apt-get update && apt-get install -y \
+    git \
+    docker \
+    docker-compose-plugin \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+#git clone https://github.com/cbrown987/cs191capstone.git
+cd cs191capstone/deployment || return
+
+docker-compose up -d --build
+
+
+
+
+
+
