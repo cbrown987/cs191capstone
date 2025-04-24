8684cf88fc76d87e299c8d650a94f342684aad87
cooperb <bcooper987@protonmail.ch>
2025-04-24 10:50:23 -0500

    experimenting

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -45,7 +45,6 @@ export async function getApi(url: string, revalidateSeconds?: number): Promise<a
     console.log(`Fetched ${fullUrl}}`);
     let r_json = await response.json();
     console.log("JSON:", r_json);
-    con
     return await r_json;
    } catch (error) {
      console.error(`Error fetching ${fullUrl}:`, error);
